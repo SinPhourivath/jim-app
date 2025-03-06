edb8cc236668b93d65c9f751e157da3a59bf6d97
unknown <sinphourivath@gmail.com>
2025-03-06 18:43:58 +0700

    add fonts and GetX state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get/get.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
 
-  runApp(const MyApp());
+  runApp(GetMaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
